4b2f510c1a3f8ea7169c0b87f9637bdb90466cd6
Daniel Singletary <dsingletary@gmail.com>
2023-04-03 22:22:58 -0500

    Update scorekeeper.js
    Added comments

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,4 +1,4 @@
-/*Code Along while watching https://www.udemy.com/the-web-developer-bootcamp*/
+// Code Along while watching https://www.udemy.com/the-web-developer-bootcamp
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
 var resetButton = document.getElementById("reset");
@@ -11,42 +11,47 @@ var p2Score = 0;
 var gameOver = false;
 var winningScore = 5;
 
+// Add click event listener to player 1 button
 p1Button.addEventListener("click", function(){
-	if(!gameOver){
-		p1Score++;
-		if(p1Score === winningScore){
-			p1Display.classList.add("winner");
-			gameOver = true;
-		}
-	}
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+            p1Display.classList.add("winner");
+            gameOver = true;
+        }
+    }
 });
 
+// Add click event listener to player 2 button
 p2Button.addEventListener("click", function(){
-	if(!gameOver){
-		p2Score++;
-		if(p2Score === winningScore){
-			p2Display.classList.add("winner");
-			gameOver = true;
-		}
-	}
+    if(!gameOver){
+        p2Score++;
+        if(p2Score === winningScore){
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+    }
 });
 
+// Add click event listener to reset button
 resetButton.addEventListener("click", function(){
-	reset();
+    reset();
 });
 
+// Reset function to reset the game
 function reset(){
-	p1Score = 0;
-	p2Score = 0;
-	p1Display.textContent = 0;
-	p2Display.textContent = 0;
-	p1Display.classList.remove("winner");
-	p2Display.classList.remove("winner");
-	gameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
 }
 
+// Add change event listener to input field to change the winning score
 numInput.addEventListener("change", function(){
-	winningScoreDisplay.textContent = this.value;
-	winningScore = Number(this.value);
-	reset();
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
+    reset();
 });
